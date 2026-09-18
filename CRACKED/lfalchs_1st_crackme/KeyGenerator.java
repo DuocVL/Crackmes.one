d7bae9a34a079bebf67f095610f8c70c1fee8e06
DuocVL <leduochhnd@gmail.com>
2026-09-18 21:15:12 +0700

    Finish challenge lfalchs_1st_crackme

---
+++ CRACKED/lfalchs_1st_crackme/KeyGenerator.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class KeyGenerator {
+    public static String generate(String name) {
+
+        char[] a = {
+            '0', '1', '2', '3', '4', '5', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z'
+        };
+
+        String b = "LFalch";
+
+        Random random = new Random(name.hashCode() | b.hashCode());
+
+        String serial = "";
+
+        for (int i = 0; i < 16; i++) {
+            serial += a[random.nextInt(a.length)];
+        }
+
+        return serial;
+    }
+    public void main(){
+        // char[] a = {'0', '1', '2', '3', '4', '5', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        // String b = "LFalch";
+
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.print("Nhập dữ liệu bạn muốn dùng để sinh key mã hóa:");
+        // String plain = scanner.nextLine();
+
+        // Random random = new Random(plain.hashCode() | b.hashCode());
+        // String key = "";
+        // for(int i = 0; i < 16;i++){
+        //     key = String.valueOf(key) + a[random.nextInt(a.length)];
+        // }
+
+        // System.out.println("Dữ liệu đã nhập:" + plain);
+        // System.out.println("Key:" + key);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Serial: " + generate(name));
+    }
+}
